1d1c3061b75d099fad550da6732e61331bfc588e
Tchi <tchi.devica@gmail.com>
2023-01-04 16:32:51 +0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { type NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <button className="btn btn-xs">Test</button>
+    </div>
+  );
+};
+
+export default Home;
