58da7346d907a862d31d6e69c4b46ef827caa0d9
Tophy <tophyrakotomavo@gmail.com>
2023-01-05 17:09:29 +0300

    example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
+import React from "react";
+
 import { type NextPage } from "next";
+
+import Footer from "../components/Footer"
+import Example from "../components/Example";
+import ExampleChild from "../components/ExampleChild";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <h1>Home</h1>
-      <button className="btn btn-xs">Test</button>
+      <ExampleChild title="Address">
+        Element...
+        Element 2 ....
+      </ExampleChild>
+      <ExampleChild title="Titre">
+        Titre elemeny...
+      </ExampleChild>
+      <ExampleChild title="Titre">
+        Titre elemeny...
+      </ExampleChild>
+      <Example title="Andy" />
+      <Footer />
     </div>
   );
 };
