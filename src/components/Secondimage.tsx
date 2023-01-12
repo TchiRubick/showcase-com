a2ac68a3a01a36c9f2f37d9410615f451a4846f9
anderson suiriex <andersonsuiriex@gmail.com>
2023-01-12 20:20:33 +1300

    last modification secondimage

--- src/components/Secondimage.tsx
+++ src/components/Secondimage.tsx
@@ -9,9 +9,19 @@ const Secondimage: React.FC = () => {
   
   return (
       <div className=" mt-20  grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-3 gap-0">
-        <div><Image src={desk} alt="logoImage"  className="w-full h-full object-cover "/> </div>
-        <div><Image src={bed} alt="logoImage"  className="w-full h-full object-cover"/></div>
-        <div><Image src={outdoor} alt="logoImage"  className="w-full h-full object-cover"/></div>
+        <div className="relative text-center">
+          <Image src={desk} alt="logoImage"  className="w-full h-full object-cover "/>
+          <div className="absolute top-48 left-48 "><button className="w-20 h-20 rounded-full bg-blue-500  text-white  ">+</button></div>
+          <div className="absolute bottom-10 left-10 text-black font-bold">Outdoor Tarrace</div> 
+        </div>
+        <div className="relative text-center">
+          <Image src={bed} alt="logoImage"  className="w-full h-full object-cover"/>
+          <div className="absolute bottom-10 left-10 text-black font-bold">Author's Landscaping</div>
+        </div>
+        <div className="relative text-center">
+          <Image src={outdoor} alt="logoImage"  className="w-full h-full object-cover"/>
+          <div className="absolute bottom-10 left-10 text-black font-bold">Enclosed Area</div>
+        </div>
       </div>
 
     );
