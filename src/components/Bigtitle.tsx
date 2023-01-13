be2a7627eb3302944e71f3022c8f99e8bc97fab7
Tophy <tophyrakotomavo@gmail.com>
2023-01-13 14:05:04 +0300

    testchange

--- src/components/Bigtitle.tsx
+++ src/components/Bigtitle.tsx
@@ -4,7 +4,7 @@ const Bigtitle: React.FC = () => {
   
   return (
       <div>
-          <div className=" pt-20 flex justify-center text-7xl font-bold text-black">
+          <div className=" pt-20 flex justify-center text-7xl font-bold text-black dark:text-white">
             <h1>FIND YOUR <br/>PLACE DREAM</h1>
           </div>
         
--- src/pages/letstalk.tsx
+++ src/pages/letstalk.tsx
@@ -7,21 +7,30 @@ const letstalk: React.FC = () => {
     <div>
       <div>
         <Header/>
-      </div>
-      <div className="bg-slate-500 shadow-current drop-shadow-lg rounded w-96 h-96 flex flex-col mx-auto place-content-center space-y-6">
-        <div className="mx-auto">
-          <input 
-            type="text" 
-            placeholder="" 
-            className="input input-bordered w-96 max-w-xs" 
+      </div> 
+      <div className="flex flex-nowrap drop-shadow-lg place-content-center h-96 rounded sm:w-auto">
+        <div className="">
+          <img className="w-52 h-96 object-cover" src="https://sb.kaleidousercontent.com/67418/604x802/8d3b057fdf/people1-bg22.jpg" alt="coco" 
           />
         </div>
-        <div className="mx-auto">
-          <button className="btn  md:btn-md lg:btn-lg">
-            Login
-          </button>
+        <div 
+          style={{
+            backgroundImage:'url("https://images.iphonephotographyschool.com/22682/How-To-Blur-Background-On-iPhone.jpg")'
+          }}
+          className=" bg-cover flex flex-col place-content-center space-y-6">
+          <div className="form-control w-96 max-w-xs">
+            <label className="label">
+              <span className="label-text text-violet-700 font-semibold">What is your name?</span>
+            </label>
+            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+          </div>
+          <div className="mx-auto">
+            <button className="btn  md:btn-md lg:btn-lg">
+              Click
+            </button>
+          </div>
         </div>
-      </div>
+      </div> 
       <div>
         <Footer/>
       </div>
