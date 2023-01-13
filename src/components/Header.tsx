fe964c8643b483c1083ba8116c2ad4de8eeaa963
anderson suiriex <andersonsuiriex@gmail.com>
2023-01-13 23:58:39 +1300

    tailwind and header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Link from "next/link";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 const Header: React.FC = () => {
   
   return (
     <div>
       <div className="flex justify-between mt-6 font-sans">
-        <a href="#" className="flex ml-10"> <p className="font-bold text-2xl ">TRIOLI</p></a>
+       <Link  href="/"> <p className="font-bold text-2xl flex ml-10 ">TRIOLI</p></Link> 
         <div className="flex justify-center space-x-8 font-bold">     
-          <div className="mt-1  h-0 w-0 border-y-8 border-y-transparent border-l-[6px] border-l-blue-600 "></div><a href="#">Home</a>
+        <div className="mt-1  h-0 w-0 border-y-8 border-y-transparent border-l-[6px] border-l-blue-600 "></div><Link  href="/">Home</Link> 
             <a href="#">Project </a>
             <a href="#">Blog</a>
             <a href="#">Contact Us</a>
+            <FaMoon/> 
           </div>
           <Link
               href="/letstalk" 
