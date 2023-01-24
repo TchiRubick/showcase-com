07d55e62636649659129d72052368088f1d8eb97
Tophy <tophyrakotomavo@gmail.com>
2023-01-24 21:44:29 +0300

    tophy

--- src/components/Bigtitle.tsx
+++ src/components/Bigtitle.tsx
@@ -4,13 +4,8 @@ const Bigtitle: React.FC = () => {
   
   return (
       <div>
-<<<<<<< HEAD
-          <div className=" pt-20 flex justify-center text-7xl font-bold text-black dark:text-white">
-            <h1>FIND YOUR <br/>PLACE DREAM</h1>
-=======
           <div className=" pt-20 flex justify-center text-7xl font-bold text-black">
             <h1 className="text-black dark:text-slate-100">FIND YOUR <br/>PLACE DREAM</h1>
->>>>>>> 9e8e4382d125c6c2483143591be6b130f496c810
           </div>
         
       </div>
--- src/pages/letstalk.tsx
+++ src/pages/letstalk.tsx
@@ -2,34 +2,26 @@ import React, { useState, useEffect, type SyntheticEvent } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
+const formatEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
 const Letstalk: React.FC = () => {
   const [email, setEmail] = useState<string>('Tophy');
   const [active, setActive] = useState<boolean>(false);
-  const [counter, setCounter] = useState<number>(0);
-
-
-  const validateEmail = () =>{
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+  
+  const validEmail = () => {
+    if (formatEmail.test(email))
     {
       setActive(true);
     }
-      else
-      {
-        setActive(false);
-      }
+    else
+    {
+      setActive(false);
+    }
   };
-
+  
   useEffect(() => {
-    setCounter(counter + 1);
-      if (email) 
-      {
-        setActive(true);
-      } 
-        else 
-        {
-          setActive(false);
-        }
-  },[validateEmail]);
+    validEmail();
+  },[email]);
 
   const handleChange = (e: SyntheticEvent) => {
     const { target } = e;
@@ -46,18 +38,17 @@ const Letstalk: React.FC = () => {
       <div>
         <Header/>
       </div> 
-      {counter}
       <div className="flex flex-nowrap place-content-center h-96 sm:w-auto  ">
-        <div className="flex flex-col place-content-center bg-white bg-opacity-5 backdrop-blur-lg space-y-6">
+        <div className="flex flex-col place-content-center bg-white bg-opacity-5 backdrop-blur-lg space-y-6 rounded-lg">
           <div className="form-control w-96 max-w-xs">
             <label className="label">
-              <span className="label-text text-lg text-violet-700 font-bold">
+              <span className="label-text text-lg text-blue-300 font-bold">
                 Adress Email
               </span>
             </label>
             <input 
               type="text" 
-              placeholder="info@gmail.com" 
+              placeholder="info@gmail.com"
               className="input input-info w-3/4 max-w-xs bg-inherit mx-auto" 
               value={email}
               onChange={handleChange}
@@ -67,7 +58,7 @@ const Letstalk: React.FC = () => {
             <button 
               className="btn btn-sm md:btn-sm lg:btn-sm bg-indigo-800 hover:bg-indigo-600 h-24" 
               onClick={handleClick} 
-              disabled={!active}>
+              disabled={!formatEmail.test(email)}>
               Click
             </button>
           </div>
